2665f28d487476e761b9ec8c879a6b52a7a543f2
Naveen <naveenudai26@gmail.com>
2026-02-04 12:27:38 +0530

    [Naveen]: Add: UC4: Implemented logic to identify high-performance vessels based on max average speed

--- src/utils/Util.java
+++ src/utils/Util.java
@@ -22,15 +22,44 @@ public class Util {
     public List<Vessel> getVesselList() {
         return vesselList;
     }
+
+    // UC3: Retrieve Vessel by ID
     public Vessel getVesselById(String vesselId) {
 
         for (Vessel vessel : vesselList) {
             if (vessel.getVesselId().equals(vesselId)) {
-                return vessel;   // Vessel found
+                return vessel;
             }
         }
 
-        return null;  // Vessel not found
+        return null;
     }
 
+    // UC4: Identify High-Performance Vessels
+    public List<Vessel> getHighPerformanceVessels() {
+
+        List<Vessel> result = new ArrayList<>();
+
+        if (vesselList.isEmpty()) {
+            return result;
+        }
+
+        // Step 1: Find maximum speed
+        double maxSpeed = vesselList.get(0).getAverageSpeed();
+
+        for (Vessel vessel : vesselList) {
+            if (vessel.getAverageSpeed() > maxSpeed) {
+                maxSpeed = vessel.getAverageSpeed();
+            }
+        }
+
+        // Step 2: Collect all vessels with max speed
+        for (Vessel vessel : vesselList) {
+            if (vessel.getAverageSpeed() == maxSpeed) {
+                result.add(vessel);
+            }
+        }
+
+        return result;
+    }
 }
